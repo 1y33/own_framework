1895e34d876b5507e74adbea2040ce8e504ea73c
1y33 <catalin.milu.2004@gmail.com>
2025-06-26 01:08:28 +0300

    Implement training loop and loss calculation in Trainer class

--- trainer.py
+++ trainer.py
@@ -27,3 +27,29 @@ class Config:
 ### loop
 # loss function
 
+class Trainer :
+    def __init__(self, config):
+        
+        self.config = config
+        
+    def train_step(self,intput,labels):
+
+        loss = torch.nn.functional.cross_entropy(input,labels)
+
+        return loss
+    
+    def training_loop(self):
+        optimizer = None
+        dataloader = None
+        self.total_loss = []
+        
+        for epoch in self.config.epochs:
+            self.epoch_loss = []
+            for input,output in dataloader:
+                ## assume input and output on the same device ( we will map it later ) 
+                optimizer.zero_grad()
+                
+                loss = self.train_step(input,output)
+                
+                loss.backward()
+                optimizer.step()
